d4caf9fe7187cee40d8874085b619507955a2d98
Katawut <katawut.u@sabuysolutions.com>
2023-06-13 14:57:12 +0700

    create socker server

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const server = require('http').createServer();
+const io = require('socket.io')(server);
 
 const PORT = 3000;
 
 server.listen(PORT);
 console.log(`Listening on port ${PORT}...`);
+
+/* create event listener for listen client connect and receive
+ a socket to communication with client. */
+io.on('connection', (socket) => {
+    console.log('a user connected.');
+});
